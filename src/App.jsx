2fd1d7d7ea484890b47ae2e4863e7cce41850ff8
Nazgul <kubanychbekovan6@gmail.com>
2022-10-15 17:27:37 +0300

    add autorization functionality

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+	return (
+		<div className='App'>
+			<p>
+				Проект: <b>TO-DO APP</b>
+			</p>
+			<p>
+				Разработчик №1: <b>Джонни</b>
+				<span>написал код авторизации</span>
+			</p>
+		</div>
+	)
+}
+
+export default App
